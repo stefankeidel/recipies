5138163b54d95f70617c99087ca130a4398c2839
Stefan <1188614+stefankeidel@users.noreply.github.com>
2021-02-15 17:09:49 +0100

    db change

--- database.py
+++ database.py
@@ -2,14 +2,14 @@
 from mongoengine import connect
 from models import Recipe, Ingredient
 
-# You can connect to a real mongo server instance by your own.
 connect("recipies", host="localhost", alias="default")
 
 
 def init_db():
-    # Create the fixtures
+    # Create some fixtures
     milch = Ingredient(name="Milch", quantity=0.5, unit="l")
-    milch.save()
+    senf = Ingredient(name="Senf", quantity=2, unit="tsp")
+    #milch.save()
 
-    eier_mit_senf = Recipe(name="Eier mit Senfsauce", ingredients=[milch])
+    eier_mit_senf = Recipe(name="Eier mit Senfsauce", ingredients=[milch, senf])
     eier_mit_senf.save()
